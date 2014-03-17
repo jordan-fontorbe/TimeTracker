09463e4d9ebfeb76fd6af8e58c75b9a7cf49afc5
nauja <jeremymorosi@hotmail.fr>
2014-03-17 18:36:51 +0100

    Cellule personnalisée

---
+++ TimeTracker/TimeTracker/TTTaskCell.h
@@ -0,0 +1,15 @@
+//
+//  TTTaskCell.h
+//  TimeTracker
+//
+//  Created by Jérémy Morosi on 17/03/2014.
+//  Copyright (c) 2014 Jojeredamyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTTaskCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
